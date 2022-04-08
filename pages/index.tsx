760c30a02c8a896a4e2a84b697a1e0265224509e
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-08 20:12:34 +0300

    Add carousel to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
 import type { NextPage } from "next";
-import { useState } from "react";
-import Blog from "../types/blog";
-import supabase from "../utils/supabase";
+import Carousel from "../components/Carousel";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Carousel
+      pages={[
+        {
+          backgroundImage:
+            "https://ceamlmpfvaqkmhrnccjo.supabase.co/storage/v1/object/public/images/rapture_uorg_2022",
+          title: "Rapture Robotics",
+          description:
+            "We are Rapture Autonomous Underwater Systems team. Our teams consists of high school students. If you want to follow us and keep up to date with developments you can follow us in our social media accounts.",
+          readMoreSource: "https://rapturerobotics.com",
+        },
+        {
+          backgroundImage:
+            "https://ceamlmpfvaqkmhrnccjo.supabase.co/storage/v1/object/public/images/rapture_uorg_2022",
+          description: "Rapture Team",
+          title: "Rapture Robotics",
+          readMoreSource: "https://rapturerobotics.com",
+        },
+      ]}
+    />
+  );
 };
 
 export default Home;
