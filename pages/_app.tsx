fba55ef327d2cb2f29c3c89a52bfa96ac2b063b6
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-08 16:15:45 +0300

    Replace mantine with tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,8 @@
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { MantineProvider } from "@mantine/core";
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App(props: AppProps) {
-  const { Component, pageProps } = props;
-
-  return (
-    <>
-      <Head>
-        <title>Site title</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-
-      <MantineProvider withGlobalStyles withNormalizeCSS>
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import Blog from "../types/blog";
 import supabase from "../utils/supabase";
 
-const Home: NextPage = () => {};
+const Home: NextPage = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
 export default Home;
