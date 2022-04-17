62e54171d267cbf6c45e11e1d986a90b113e90b1
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-17 23:33:06 +0300

    Add font family options to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ["Work Sans", "sans"],
+        akshar: ["Akshar", "sans"],
+      },
+      colors: {},
+    },
   },
   plugins: [],
 };
