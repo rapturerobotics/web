6b287ee29f0ecc297c2d40adef36b4d31d28ad5d
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-17 23:35:07 +0300

    Customize document component for tailwind theme

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html className="bg-slate-900 text-slate-100 font-akshar">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
