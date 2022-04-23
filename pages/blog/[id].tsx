71c0c1ce8a466957b2c7e96136187e7f809b34d5
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-23 10:08:38 +0300

    Add footer to BlogPostPage

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
+import Footer from "../../components/Footer";
 import useBlog from "../../hooks/useBlog";
 
 const BlogPostPage = () => {
@@ -18,22 +19,26 @@ const BlogPostPage = () => {
         <title>{blog.title}</title>
       </Head>
 
-      <div
-        className="w-full h-[33vh] bg-center bg-cover bg-no-repeat"
-        style={{ backgroundImage: `url(${blog.background_image})` }}
-      />
+      <div className="w-full flex flex-col items-center min-h-screen">
+        <div
+          className="w-full h-[33vh] bg-center bg-cover bg-no-repeat"
+          style={{ backgroundImage: `url(${blog.background_image})` }}
+        />
 
-      <div className="w-full flex flex-col items-center">
-        <div className="w-full max-w-3xl p-4 pt-16 flex flex-col gap-4">
-          <h1 className="text-6xl font-semibold mb-8 font-work text-center">
-            {blog.title}
-          </h1>
+        <div className="w-full flex flex-col items-center">
+          <div className="w-full max-w-3xl p-4 pt-16 flex flex-col gap-4">
+            <h1 className="text-6xl font-semibold mb-8 font-work text-center">
+              {blog.title}
+            </h1>
 
-          <div className="prose prose-invert max-w-full">
-            <ReactMarkdown>{blog.content}</ReactMarkdown>
+            <div className="prose prose-invert max-w-full">
+              <ReactMarkdown>{blog.content}</ReactMarkdown>
+            </div>
           </div>
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
