27f046d7f8040444763fb29865580b56667870bd
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-23 11:16:21 +0300

    Add footer to BlogHomePage

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import BlogCard from "../../components/BlogCard";
+import Footer from "../../components/Footer";
 import useBlogs from "../../hooks/useBlogs";
 
 const BlogHomePage: NextPage = () => {
@@ -12,7 +13,7 @@ const BlogHomePage: NextPage = () => {
         <title>Rapture Blog</title>
       </Head>
 
-      <div className="w-full max-w-5xl p-4 pt-16 flex flex-col gap-4">
+      <div className="w-full max-w-5xl p-4 pt-16 flex flex-col gap-4 min-h-screen">
         <h1 className="text-6xl font-semibold mb-8 font-work">Rapture Blog</h1>
 
         {isLoading ? (
@@ -25,6 +26,8 @@ const BlogHomePage: NextPage = () => {
           </section>
         )}
       </div>
+
+      <Footer />
     </div>
   );
 };
