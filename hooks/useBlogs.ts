df33db5b354e08730ca0944fcc901fef006ba8c4
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-23 08:16:27 +0300

    Create useBlogs react-query hook

---
+++ hooks/useBlogs.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query";
+import { PreviewedBlog } from "../types/blog";
+import supabase from "../utils/supabase";
+
+const fetchBlogs = async () => {
+  const { data: blogs, error } = await supabase.from<PreviewedBlog>("blogs")
+    .select(`
+      id, created_at, title, description, background_image,
+      author ( id, name ), tags ( id, label )
+    `);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (!blogs) {
+    throw new Error("Users not found");
+  }
+
+  return blogs;
+};
+
+const useBlogs = () => useQuery("blogs", fetchBlogs);
+
+export default useBlogs;
