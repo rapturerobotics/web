92b12ad9a57ecd4e738b6dd9bca5240142ac1210
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-25 18:58:00 +0300

    Add Header to custom Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Header from "../components/Header";
 
 export default function Document() {
   return (
     <Html className="bg-slate-900 text-slate-100 font-akshar">
       <Head />
-      <body>
+      <body className="flex flex-col">
+        <Header />
         <Main />
         <NextScript />
       </body>
