a8c36fb7521284691201a64ffcebc4c5fcd8a338
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-25 21:05:32 +0300

    Create reusable custom h1 component

---
+++ components/Header1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header1: React.FC = ({ children }) => {
+  return <h1 className="text-6xl font-semibold mb-8 font-work">{children}</h1>;
+};
+
+export default Header1;
