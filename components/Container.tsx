8e2b062273a6b790b4677ddcc4d5239a4dd9c00b
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-29 13:26:15 +0300

    Create Container component

---
+++ components/Container.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classNames from "classnames";
+
+interface ContainerProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const Container: React.FC<ContainerProps> = ({
+  children,
+  className,
+  style,
+}) => {
+  const mergedClassName = classNames(
+    "flex flex-col w-full max-w-5xl pt-16 p-6 mx-auto font-work",
+    className
+  );
+
+  return (
+    <div className={mergedClassName} style={style}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
