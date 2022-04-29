205b65e64216e09e10b4a2ba4c8edaed9a957fe2
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-29 18:10:59 +0300

    Add spacing to LoadingIndicator

--- components/LoadingIndicator.tsx
+++ components/LoadingIndicator.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingIndicator: React.FC = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center space-x-2 animate-pulse">
+    <div className="w-full h-full flex items-center justify-center space-x-2 animate-pulse p-32 m-auto">
       <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
       <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
       <div className="w-8 h-8 bg-blue-400 rounded-full"></div>
