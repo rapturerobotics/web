301a4f01617581b1d0af390ee86adc28837a38ae
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-05-15 14:27:52 +0300

    Seperate IntroductionBlock into its own component in homepage

---
+++ components/IntroductionBlock.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const IntroductionBlock: React.FC = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8 py-8 pb-16">
+      <div>
+        <h1 className="text-7xl font-semibold mb-8 font-work">
+          Rapture Robotics
+        </h1>
+
+        <p className="text-lg font-akshar">
+          We are an Autonomous Surface and Underwater Systems team of future
+          engineers. Stay tune for the updates!
+        </p>
+      </div>
+
+      <img
+        src="https://ceamlmpfvaqkmhrnccjo.supabase.co/storage/v1/object/public/images/rapture_logo_transparent_1024x1024.png"
+        alt="Rapture logo with pulse animation"
+        className="animate-pulse p-8"
+      />
+    </div>
+  );
+};
+
+export default IntroductionBlock;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import IntroductionBlock from "../components/IntroductionBlock";
 import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
@@ -10,25 +11,8 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="flex justify-center">
-        <div className="flex flex-col justify-between w-full max-w-5xl pt-16 p-8 md:min-h-screen">
-          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
-            <div>
-              <h1 className="text-7xl font-semibold mb-8 font-work">
-                Rapture Robotics
-              </h1>
-
-              <p className="text-lg font-akshar">
-                We are an Autonomous Surface and Underwater Systems team of
-                future engineers. Stay tune for the updates!
-              </p>
-            </div>
-
-            <img
-              src="https://ceamlmpfvaqkmhrnccjo.supabase.co/storage/v1/object/public/images/rapture_logo_transparent_1024x1024.png"
-              alt="Rapture logo with pulse animation"
-              className="animate-pulse p-8"
-            />
-          </div>
+        <div className="flex flex-col justify-between w-full max-w-5xl md:min-h-screen gap-8">
+          <IntroductionBlock />
 
           <Footer />
         </div>
